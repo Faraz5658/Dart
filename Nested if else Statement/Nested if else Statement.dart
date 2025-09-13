a5394cdd118ee127a994a59133e5449d8dd5457b
Faraz5658 <farazahmad5623@gmail.com>
2025-09-13 10:44:48 +0500

    nested if else statement

---
+++ Nested if else Statement/Nested if else Statement.dart
@@ -0,0 +1,14 @@
+void main() {
+  int a = 20;
+  if (a < 10) {
+    print("Less than 10");
+  } else if (a < 20) {
+    print("Less than 10");
+  } else if (a < 30) {
+    print("Less than 30");
+  } else if (a < 40) {
+    print("Less than 40");
+  } else {
+    print("Not Valid");
+  }
+}
