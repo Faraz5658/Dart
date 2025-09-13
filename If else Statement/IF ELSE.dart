1789571445be0530be1bef3dfd393f7cce5d2daf
Faraz5658 <farazahmad5623@gmail.com>
2025-09-13 10:44:04 +0500

    if else statement

---
+++ If else Statement/IF ELSE.dart
@@ -0,0 +1,17 @@
+void main() {
+  int a = 10;
+  if (a == 10) {
+    print("TRUE");
+  } else {
+    print("False");
+  }
+
+  print("*********************");
+
+  int b = 20;
+  if (b == 10) {
+    print("TRUE");
+  } else {
+    print("False");
+  }
+}
