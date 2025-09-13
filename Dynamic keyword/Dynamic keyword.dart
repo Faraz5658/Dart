555c1140dcf7ef5dbef5dfc5d404fbcf161edc00
Faraz5658 <farazahmad5623@gmail.com>
2025-09-13 09:58:45 +0500

    Dynamic keyword

---
+++ Dynamic keyword/Dynamic keyword.dart
@@ -0,0 +1,22 @@
+void main() {
+  print("***********************");
+  dynamic a = 'FARAZ BANGASH';
+  print(a);
+  print(a.runtimeType);
+  print("***********************");
+
+  a = 56;
+  print(a);
+  print(a.runtimeType);
+  print("***********************");
+
+  a = 2.5;
+  print(a);
+  print(a.runtimeType);
+  print("***********************");
+
+  a = true;
+  print(a);
+  print(a.runtimeType);
+  print("***********************");
+}
