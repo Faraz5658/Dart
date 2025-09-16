0c75c0fb895ba2e7e1cfc932e02edbbfdfdbb7f1
Faraz5658 <farazahmad5623@gmail.com>
2025-09-16 19:02:20 +0500

    Different between____Do_While_loop - While_loop

---
+++ Different between____Do_While_loop - While_loop/Do_While_loop.dart
@@ -0,0 +1,15 @@
+void main() {
+  print("*** Do while loop***");
+  int b = 2;
+  do {
+    print("FARAZ BANGASH");
+    b++;
+  } while (b <= 2);
+
+  print("***while loop***");
+  int a = 1;
+  while (a < 4) {
+    print("BANGASH"); //not a print
+    a++;
+  }
+}
