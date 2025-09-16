9c210c3e9418f2391c01bc10c86953fc868dcd44
Faraz5658 <farazahmad5623@gmail.com>
2025-09-16 19:01:00 +0500

    Add include for_loop

---
+++ include For_loop/For_loop include for loop.dart
@@ -0,0 +1,9 @@
+void main() {
+  for (var a = 0; a <= 2; a++) {
+    print("THE VLAUE OF I : $a");
+    for (var b = 0; b <= 2; b++) {
+      print("THE VLAUE OF J : $b");
+    }
+    print("******************");
+  }
+}
