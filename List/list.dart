33b19e434c5346c5d034aed929767c623ea1d557
Faraz5658 <farazahmad5623@gmail.com>
2025-09-17 15:54:26 +0500

    List Add in dart

---
+++ List/list.dart
@@ -0,0 +1,10 @@
+void main() {
+  List Num;
+  Num = [12, 23, 34, 45, 56, 67, 78];
+  print("****  Full List  ********");
+  print(Num);
+  print("*****  Target Index  *********");
+  print(Num[2]);
+  print("****  Length  **********");
+  print(Num.length);
+}
