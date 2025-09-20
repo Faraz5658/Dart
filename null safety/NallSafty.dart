d567f0e6ab9f24f12539c7795c0e7460a8a15494
Faraz5658 <farazahmad5623@gmail.com>
2025-09-20 09:02:18 +0500

    null safty in dart

---
+++ null safety/NallSafty.dart
@@ -0,0 +1,19 @@
+void main() {
+  int num;
+  print(num); // this non-nullable
+
+  print("****** String *********");
+  String? myName;
+  print(myName); // this is null
+  myName = "B A N G A S H";
+  print(myName);
+
+  print("****** Number *********");
+
+  int Num = 57;
+  print(Num);
+  Num = 56;
+  print(Num);
+
+  print("***************");
+}
