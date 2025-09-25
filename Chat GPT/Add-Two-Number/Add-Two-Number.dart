c6607db9500c5b884ecfb38b2e457c53482e7342
Faraz5658 <farazahmad5623@gmail.com>
2025-09-25 13:01:21 +0500

    Add Two Number

---
+++ Chat GPT/Add-Two-Number/Add-Two-Number.dart
@@ -0,0 +1,13 @@
+import 'dart:io';
+
+void main() {
+  //ENTER 1st NUMBER
+  stdout.write("ENTER 1st NUMBER : ");
+  int num1 = int.parse(stdin.readLineSync()!);
+
+  //ENTER 2nd NUMBER
+  stdout.write("ENTER 1st NUMBER : ");
+  int num2 = int.parse(stdin.readLineSync()!);
+  int Sum = num1 + num2;
+  print("THE SUM TWO IS : $Sum");
+}
